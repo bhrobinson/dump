363e4c312eeea9be6ac9aac019f806c16351a722
Houston Robinson <Brandon.Robinson@centene.com>
2023-03-10 07:57:28 -0600

    added

---
+++ CertCheck/filter.py
@@ -0,0 +1,6 @@
+import json
+
+file = "| f066ffdc-a18e-4221-a5a9-fd5fe4581106 | Arizona EVV                                     | /arizonaevv                          |    1.0.0 | CN=wildcard.prod2.us-east-1.sandata.com                                        | 2023-03-10 19:54 | 2023-02-24 19:54 | 5C:26:D5:AA:FD:B3:3A:93:EC:D0:5D:3F:DC:EB:A7:35 |"
+#| f066ffdc-a18e-4221-a5a9-fd5fe4581106 | Arizona EVV                                     | /arizonaevv                          |    1.0.0 | CN=wildcard.prod2.us-east-1.sandata.com                                        | 2023-03-10 19:54 | 2023-02-24 19:54 | 5C:26:D5:AA:FD:B3:3A:93:EC:D0:5D:3F:DC:EB:A7:35 |
+#| ea37edba-7c04-428f-b996-8f241b6cbf7f | MHNAuthTokenV2 ZScaler                          | /mhn/v2/auth-z                       |    1.0.0 | CN=*.apiportal.akana.com, O="Perforce Software, Inc.", L=Alameda,              | 2023-02-16 17:59 | 2022-02-02 18:00 | 47:99:A9:C1:8A:FC:8A:C0:7B:83:C1:FA:D0:04:93:FB |
+print(file)
